3308bb5f17484e45045f07b8bc4471171c3dc28d
Akshay Jaiswal <akshay.jaiswal@sjsu.edu>
2018-11-17 19:26:48 -0800

    Initial commit for box jump project

---
+++ BoxJump/Assets/CodeBase/jumpBox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpBox : MonoBehaviour {
+
+    public KeyCode moveL;
+    public KeyCode moveR;
+
+    public float horizVel = 0;
+    public int laneNum = 2;
+    public string controllocked = "n";
+
+	// Use this for initialization
+	void Start () {
+		// this.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * 30f,  ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+    }
+
+}
